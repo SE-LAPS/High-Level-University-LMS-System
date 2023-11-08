ca2a8c1d2e605d452c39c112db8cf471fa6fb234
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-11-09 01:05:20 +0530

    Create Courses.php

---
+++ pages/Courses.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Available Courses</title>
+        <link rel="stylesheet" href="../css/Courses.scss"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    </head>
+    <body class="body">
+        <?php require '../include/VisitorNav.php'; ?>
+        <div class="container mt-4 container1">
+            <h6>Available Courses</h6>
+            <div class="container mt-4 container2">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <img src="../img/Dance Slide 1.jpg" class="card-img-top" alt="Course 1">
+                            <div class="card-body">
+                                <h5 class="card-title">WAD</h5>
+                                <div class="d-flex align-items-center">
+                                    <img src="../img/person2.jpg" class="profile-image-small" alt="Tutor 1">
+                                    <span class="ml-2">Nimal</span>
+                                </div>
+                                <p class="card-text text-muted"> 
+                                    <small>Web Development</small>
+                                </p>
+                                <div class="text-right">
+                                    <a href="#" class="btn btn-primary">View</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <img src="course2.jpg" class="card-img-top" alt="Course 2">
+                            <div class="card-body">
+                                <h5 class="card-title">Course 2</h5>
+                                <div class="d-flex align-items-center">
+                                    <img src="tutor2.jpg" class="profile-image-small" alt="Tutor 2">
+                                    <span class="ml-2">Tutor 2</span>
+                                </div>
+                                <p class="card-text text-muted">Graphic Design</p>
+                                <div class="text-right">
+                                    <a href="#" class="btn btn-primary">View</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <img src="course3.jpg" class="card-img-top" alt="Course 3">
+                            <div class="card-body">
+                                <h5 class="card-title">Course 3</h5>
+                                <div class="d-flex align-items-center">
+                                    <img src="tutor3.jpg" class="profile-image-small" alt="Tutor 3">
+                                    <span class="ml-2">Tutor 3</span>
+                                </div>
+                                <p class="card-text text-muted">Data Science</p>
+                                <div class="text-right">
+                                    <a href="#" class="btn btn-primary">View</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <img src="course4.jpg" class="card-img-top" alt="Course 4">
+                            <div class="card-body">
+                                <h5 class="card-title">Course 4</h5>
+                                <div class="d-flex align-items-center">
+                                    <img src="tutor4.jpg" class="profile-image-small" alt="Tutor 4">
+                                    <span class="ml-2">Tutor 4</span>
+                                </div>
+                                <p class="card-text text-muted">Mobile App Development</p>
+                                <div class="text-right">
+                                    <a href="#" class="btn btn-primary">View</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </body>
+</html>
+
