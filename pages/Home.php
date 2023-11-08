9a6b2b26747b71cae0723b6da61fcdfd1b1a55c6
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-11-09 01:05:44 +0530

    Create Home.php

---
+++ pages/Home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/home.css" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    </head>
+    <body>
+    <?php require __DIR__ . '/../include/VisitorNav.php'; ?>
+        <div class="home-body">
+            <div class="hero-image">
+                <div class="hero-text">
+                    <h1>SkillBridge</h1>
+                    <p>Unlock Your Potential with SkillBridge
+                        <br> Share, Learn, and Master Skills Together.
+
+                    </p>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="skill-list">
+                            <h2>You need it, we've got it...</h2>
+                        </div>
+
+
+                        <div class="skill-content">
+                            <h2>Popular Skills</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    </body>
+
+</html>
