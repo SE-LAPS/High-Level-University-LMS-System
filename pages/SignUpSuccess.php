31f3def29da8e9ef590dd47704688d71d825ee9f
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-11-09 01:07:55 +0530

    Create SignUpSuccess.php

---
+++ pages/SignUpSuccess.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />        
+        <link rel="stylesheet" href="../css/SignUpSuccess.scss"/>
+        <title>Signup Successful</title>
+    </head>
+    <body>
+        <div class="msg-container">
+            <div class="container mt-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-body text-center"> <!-- Centering the content -->
+                                <span class="material-symbols-outlined" style="font-size: 3.5rem; color: #235391;">
+                                    check_circle
+                                </span>
+                                <br>
+                                <h5 class="h5">SUCCESS</h5>
+                                <p>Congratulations, your account <br> has been successfully created.</p>
+                                <div class="tokenMsg">
+                                    <p>You received 100 tokens upon signing up.</p>
+                                </div>
+                                <div class="text-center">
+                                    <a href="../pages/signIn.php" class="btn btn-success">Continue</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    </body>
+</html>
