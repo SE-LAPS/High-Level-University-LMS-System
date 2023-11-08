eae34a5433af701e6ada6b25e67a386397db0aee
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-11-09 01:05:32 +0530

    Create Dummy.php

---
+++ pages/Dummy.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leading Tutors</title>
+    <link rel="stylesheet" href="../css/LeadingTutors.scss">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body class="body">
+    <div class="container mt-4 container1">
+        <h6>Leading Tutors</h6>
+        <div class="container mt-4 container2">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="../img/person2.jpg" class="card-img-top" alt="Tutor 1">
+                        <div class="card-body">
+                            <h5 class="card-title">Tutor 1</h5>
+                            <p class="card-text">Web Development, JavaScript</p>
+                            <p class="card-text">1000 Tokens</p>
+                            <p class="card-text">Platinum</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="../img/person2.jpg" class="card-img-top" alt="Tutor 2">
+                        <div class="card-body">
+                            <h5 class="card-title">Tutor 2</h5>
+                            <p class="card-text">Graphic Design, Photoshop</p>
+                            <p class="card-text">800 Tokens</p>
+                            <p class="card-text">Gold</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="../img/person2.jpg" class="card-img-top" alt="Tutor 3">
+                        <div class="card-body">
+                            <h5 class="card-title">Tutor 3</h5>
+                            <p class="card-text">Data Science, Python</p>
+                            <p class="card-text">1200 Tokens</p>
+                            <p class="card-text">Diamond</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="../img/person2.jpg" class="card-img-top" alt="Tutor 4">
+                        <div class="card-body">
+                            <h5 class="card-title">Tutor 4</h5>
+                            <p class="card-text">Mobile App Development, Swift</p>
+                            <p class="card-text">900 Tokens</p>
+                            <p class="card-text">Silver</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
