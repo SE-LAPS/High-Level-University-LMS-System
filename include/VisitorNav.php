18a411f186afdf80f211bec039de2875d3693ce1
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-11-09 00:16:49 +0530

    Create VisitorNav.php

---
+++ include/VisitorNav.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/VisitorNav.scss" />
+        <title></title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light mx-0">
+            <div class="container">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="navbar-brand" href="../pages/Home.php">SkillBridge</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../pages/Home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Skills</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Leading Tutors</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../pages/Courses.php">Courses</a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../pages/SignIn.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../pages/SignUp.php">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </body>
+</html>
