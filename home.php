dac1479f6303ead1b3dac2193f4050c8e56f74ef
Lahiru Senavirathna <87580847+SE-LAPS@users.noreply.github.com>
2023-11-11 01:48:04 +0530

    Add home.php

---
+++ home.php
@@ -0,0 +1,16 @@
+<!-- <h1><?php echo $title;?></h1> -->
+<div class="col-lg-12"> 
+<h4>Mandate</h4>
+    <p> Carlos Hilado Memorial State College shall primarily provide higher technological, professional and vocational instruction and training in science/agricultural, in industrial fields as well as short-term technical or vocational courses. It shall provide research advance studies and progressive leadership in its areas of specialization.</p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4> 
+  <p>CHMSC EXCELS: Excellence, Competence, and Educational Leadership in Science and Technology.
+
+
+Driven by its passion for continuous improvement, the State College has to vigorously pursue distinction and proficiency in delivering its statutory functions to the Filipino people in the fields of education, business, agro-fishery, industrial, science and technology, through committed and competent human resource, guided by the beacon of innovation and productivity towards the heights of elevated status.</p>
+  <h4>Mission</h4>
+  <p> A leading green institution in higher and continuing education committed to engage in quality instruction, development-oriented research, sustainable lucrative economic enterprise, and responsive extension and training services through relevant academic programs to empower a human resource that responds effectively to challenges in life and act as catalyst in the holistic development of a humane society.</p>
+</div>
+ 
+
