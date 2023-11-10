df94036215a8d9107930c4681712a964fe5c2004
Lahiru Senavirathna <87580847+SE-LAPS@users.noreply.github.com>
2023-11-11 01:48:22 +0530

    Add index.php

--- index.php
+++ index.php
@@ -1,16 +1,55 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        require './pages/Home.php';
-        ?>
-    </body>
-</html>
+<?php 
+require_once("include/initialize.php");  
+if (!isset($_SESSION['StudentID'])) {
+  # code...
+  redirect('login.php');
+}
+$content='home.php';
+$view = (isset($_GET['q']) && $_GET['q'] != '') ? $_GET['q'] : '';
+switch ($view) { 
+  case 'lesson':
+    $title = "Lesson";
+    $content = 'lesson.php';
+   # code...
+   break; 
+  case 'exercises':
+    $title = "Exercises";
+    $content = 'exercises.php';
+   # code...
+   break; 
+  case 'download':
+    $title = "Download";
+    $content = 'download.php';
+   # code...
+   break; 
+  case 'about':
+    $title = "About Us";
+    $content = 'about.php';
+   # code...
+   break; 
+  case 'playvideo':
+    $title = "Play Video";
+    $content = 'playvideo.php';
+   # code...
+   break; 
+  case 'viewpdf':
+    $title = "Play Video";
+    $content = 'viewpdf.php';
+   # code...
+   break; 
+  case 'question':
+    $title = "Question";
+    $content = 'question.php';
+   # code...
+   break; 
+  case 'quizresult':
+    $title = "Result";
+    $content = 'quizresult.php';
+   # code...
+   break; 
+  default :
+    $title = "Home";
+    $content    = 'home.php';
+}
+require_once("theme/templates.php");
+?>
